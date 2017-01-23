d67717ea17c1c0e1cd220aff3d3b6d9ee77e7b2e
Alexey Gladyshev <alexey0gladyshev@gmail.com>
2017-01-24 02:38:28 +0300

    add comments

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
+// TBH, I am complete novice at JS, JQuery, AJAX.
+// This code might be messy, but at least I got it to work
+
 $(document).foundation();
 
 $("button[name=showMore]")
-  .click(
-       function ()
-       {
+    .click(
+       function () {
            $(this).hide();
-       }
-  );
+        }
+    );
+
+$("body").on("click", "button[name=editComment]", function() {
+      // Shows comment edit form and passes text to textarea
+      $comment = $(this).parent().parent();
+      $textarea = $comment.find(".comment__text");
+      $commit = $comment.find(".comment__commit");
+      $discard = $comment.find(".comment__discard");
+      $editarea = $comment.find(".comment__editarea");
+      text = $textarea.text();
+      $editarea.text(text);
+      $textarea.hide();
+      $commit.show();
+      $discard.show();
+      $editarea.show();
+});
+
+$("body").on("click", "button[name=comment__discard]", function () {
+      // discard changes, brings back normal comment view
+      $comment = $(this).parent().parent().parent().parent();
+      /*I got to admit, this looks lame here*/
+      $comment.find(".comment__editarea").hide();
+      $comment.find(".comment__discard").hide();
+      $comment.find(".comment__commit").hide();
+      $comment.find(".comment__text").show();
+});
+
 function Like(blog_key){
   $.ajax({
     type: "POST",
@@ -19,6 +47,7 @@ function Like(blog_key){
       $("#" + id).find(".likes__counter").text(data.likes);
   });
 }
+
 function Comment(content, blog_key){
   $.ajax({
     type: "POST",
@@ -30,6 +59,7 @@ function Comment(content, blog_key){
       $(".comment__new").append(data.comment);
   });
 }
+
 function DeleteComment(comment_key){
   $.ajax({
     type: "POST",
@@ -43,30 +73,6 @@ function DeleteComment(comment_key){
   });
 }
 
-$('body').on("click", "button[name=editComment]", function() {
-      $comment = $(this).parent().parent();
-      $textarea = $comment.find(".comment__text");
-      $commit = $comment.find(".comment__commit");
-      $discard = $comment.find(".comment__discard");
-      $editarea = $comment.find(".comment__editarea");
-      text = $textarea.text();
-      $editarea.text(text);
-      $textarea.hide();
-      $commit.show();
-      $discard.show();
-      $editarea.show();
-   }
-);
-$('body').on("click", "button[name=comment__discard]", function() {
-      $comment = $(this).parent().parent().parent().parent();
-      /*I got to admit, this looks lame here*/
-      $comment.find(".comment__editarea").hide();
-      $comment.find(".comment__discard").hide();
-      $comment.find(".comment__commit").hide();
-      $comment.find(".comment__text").show();
-   }
-);
-
 function EditComment(content, comment_key){
   $.ajax({
     type: "POST",
@@ -75,13 +81,11 @@ function EditComment(content, comment_key){
     data: JSON.stringify({"content": content, "comment_key": comment_key})
   })
   .done(function( data ) {
-      console.log("done");
       var id = data.comment_key;
-      console.log(id);
       $("#" + id).find(".comment__editarea").hide();
       $("#" + id).find(".comment__discard").hide();
       $("#" + id).find(".comment__commit").hide();
       $("#" + id).find(".comment__text").text(data.content);
       $("#" + id).find(".comment__text").show();
-  });
-};
+        });
+}
